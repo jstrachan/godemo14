e3ab551d8703a4f168b055c83c83ed4a523c9e1c
James Strachan <james.strachan@gmail.com>
2018-12-05 07:47:41 +0000

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X AWESOME17 golang http example"
+	title := "Jenkins X AWESOME18 golang http example"
 
 	from := ""
 	if r.URL != nil {
